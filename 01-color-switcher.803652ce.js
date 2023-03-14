06ff87d190db9c893175d5696b9de783c9627a13
ElizPery <113451264+ElizPery@users.noreply.github.com>
2023-03-14 12:02:23 +0000

    Deploying to gh-pages from @ ElizPery/goit-js-hw-09@19e96a14a21c37164717d9690193e0f2098474e9 🚀

---
+++ 01-color-switcher.803652ce.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector(".data - start"),stopBtn:document.querySelector([data-stop])};console.log(t.startBtn)}();
+//# sourceMappingURL=01-color-switcher.803652ce.js.map
---
+++ 01-color-switcher.850b66aa.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector(".data - start"),stopBtn:document.querySelector([data-stop])};console.log(t.startBtn);
+//# sourceMappingURL=01-color-switcher.850b66aa.js.map
