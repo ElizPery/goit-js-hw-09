c0a0ad23e489832778a96b8f0510db3c66e64fe0
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-03-14 13:44:00 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,35 @@
 const refs = {
-    startBtn: document.querySelector('.data - start'),
-    stopBtn: document.querySelector([data - stop])
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
 }
 
-console.log(refs.startBtn)
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+refs.stopBtn.disabled = true;
+timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartClick() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  changeColor();
+
+  timerId = setInterval(() => {
+    changeColor();
+  }, 1000)
+}
+
+function onStopClick() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  clearInterval(timerId);
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
 }
