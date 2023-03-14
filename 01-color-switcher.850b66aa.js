dad1e49b088b193b93d93d3f81c987868e993a67
ElizPery <113451264+ElizPery@users.noreply.github.com>
2023-03-14 12:44:33 +0000

    Deploying to gh-pages from @ ElizPery/goit-js-hw-09@c0a0ad23e489832778a96b8f0510db3c66e64fe0 🚀

--- 01-color-switcher.850b66aa.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector(".data - start"),stopBtn:document.querySelector([data-stop])};console.log(t.startBtn);
-//# sourceMappingURL=01-color-switcher.850b66aa.js.map
---
+++ 01-color-switcher.c2221bbe.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e(),timerId=setInterval((()=>{e()}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(timerId)})),t.stopBtn.disabled=!0,timerId=null;
+//# sourceMappingURL=01-color-switcher.c2221bbe.js.map
