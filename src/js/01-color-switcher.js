19e96a14a21c37164717d9690193e0f2098474e9
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-03-14 13:01:53 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
+const refs = {
+    startBtn: document.querySelector('.data - start'),
+    stopBtn: document.querySelector([data - stop])
+}
 
+console.log(refs.startBtn)
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
